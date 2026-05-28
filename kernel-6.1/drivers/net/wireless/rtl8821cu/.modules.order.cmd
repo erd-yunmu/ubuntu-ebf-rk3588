e2e0ac5a78c9423992099116dc28b4f62f187024
Egeaaaaaaaaaaaa <496625010@qq.com>
2026-05-28 06:56:20 +0000

    “关闭8811cu，添加8822cu，并且在内核配置开启”

---
+++ kernel-6.1/drivers/net/wireless/rtl8821cu/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/net/wireless/rockchip_wlan/rtl8821cu/modules.order := {   echo drivers/net/wireless/rockchip_wlan/rtl8821cu/8821cu.ko; :; } > drivers/net/wireless/rockchip_wlan/rtl8821cu/modules.order
