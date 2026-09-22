c4f2f7bd7cc3e8cba8812cd1862f75ba1b369ae6
erd-yunmu <cancet@embedfire.com>
2026-09-22 08:13:26 +0000

    fix missing dma_resv lock in GEM mmap

--- kernel-6.1/drivers/gpu/drm/drm_gem_shmem_helper.c
+++ kernel-6.1/drivers/gpu/drm/drm_gem_shmem_helper.c
@@ -613,6 +613,7 @@ int drm_gem_shmem_mmap(struct drm_gem_shmem_object *shmem, struct vm_area_struct
 	if (is_cow_mapping(vma->vm_flags))
 		return -EINVAL;
 
+	dma_resv_lock(shmem->base.resv, NULL);
 	ret = drm_gem_shmem_get_pages(shmem);
 	dma_resv_unlock(shmem->base.resv);
 
